96b77ea485b00a3f0b0a04adfeea39251fdb7c3f
cpstuardo <cpstuardo@uc.cl>
2021-12-23 15:30:34 -0300

    arreglos4

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import "./App.css";
 import ResponsiveRadar from "./components/radar";
 import dataRadar from "./data/dataRadar";
-import { Container, Col, Row } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <Container>
-        <Col>
-          <h1>Evaluación de Rendimiento Deportivo</h1>
-          <Row>
-            <img src={require("./legends.jpeg")} width={"40%"} height={"50%"} />
-          </Row>
-          <Row>
-            <ResponsiveRadar
-              data={dataRadar}
-              keys={["results"]}
-              indexBy={"item"}
-            />
-          </Row>
-        </Col>
-      </Container>
+      <h1>Evaluación de Rendimiento Deportivo</h1>
+      <img src={require("./legends.jpeg")} width={"40%"} height={"50%"} />
+      <ResponsiveRadar data={dataRadar} keys={["results"]} indexBy={"item"} />
     </div>
   );
 }
