573437d253214e7e56ffe6ca4e24405bcba69938
cpstuardo <cpstuardo@uc.cl>
2021-12-23 12:52:26 -0300

    avance

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ResponsiveRadar from "./components/radar";
+import dataRadar from "./data/dataRadar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Evaluación de Rendimiento Deportivo</h1>
+      <div className="containerRadar">
+        <ResponsiveRadar data={dataRadar} keys={["results"]} indexBy={"item"} />
+      </div>
+      <img src={require("./legends.jpeg")} width={400} height={170} />
     </div>
   );
 }
---
+++ src/data/dataRadar.js
@@ -0,0 +1,68 @@
+const dataRadar = [
+  {
+    item: "Time Level 1",
+    results: 70,
+  },
+  {
+    item: "Time Level 2",
+    results: 90,
+  },
+  {
+    item: "Total time (30-Yards)",
+    results: 50,
+  },
+  {
+    item: "Symmetry R/L Index",
+    results: 90,
+  },
+  {
+    item: "Height",
+    results: 10,
+  },
+  {
+    item: "Power",
+    results: 10,
+  },
+  {
+    item: "Contact time",
+    results: 70,
+  },
+  {
+    item: "Power density",
+    results: 50,
+  },
+  {
+    item: "Average Power",
+    results: 90,
+  },
+  {
+    item: "Average Height",
+    results: 70,
+  },
+  {
+    item: "Average Contact Time",
+    results: 50,
+  },
+  {
+    item: "Jump length",
+    results: 90,
+  },
+  {
+    item: "Contact time",
+    results: 10,
+  },
+  {
+    item: "Total time (reaction)",
+    results: 50,
+  },
+  {
+    item: "Reaction time",
+    results: 70,
+  },
+  {
+    item: "Speed of processing",
+    results: 30,
+  },
+];
+
+export default dataRadar;
